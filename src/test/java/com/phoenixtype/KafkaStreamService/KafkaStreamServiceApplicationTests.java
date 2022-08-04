d33d2257b667022abcd5b165025165a70ac03efd
Samuel Uche Akuma <samuelakuma130@gmail.com>
2022-08-03 23:50:22 -0400

    Initial commit

---
+++ src/test/java/com/phoenixtype/KafkaStreamService/KafkaStreamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixtype.KafkaStreamService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
